cddd97f2648e45175bba302736838e3b7cb23938
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-03 17:28:25 +0200

    Add note

---
+++ app/note/page.tsx
@@ -0,0 +1,36 @@
+const Note = () => (
+  <main className="flex h-screen w-screen items-center justify-center">
+    <article className="w-60 rounded-lg bg-sky-200">
+      <h1 className="px-4 pt-3 text-base font-medium">すなわち、僕だぜ！</h1>
+      <p className="px-4 pb-3 pt-3 text-sm">
+        Phone:
+        <br />
+        <br />
+        ミリシタ：
+        <br />
+        所持コイン（しょじコイン）
+        <br />
+        獲得コインボーナス（かくとくコインボーナス）
+        <br />
+        上記（じょうき）
+        <br />
+        変化します（へんかします）
+        <br />
+        の通り（のとおり）
+        <br />
+        日付（ひづけ）
+        <br />
+        更新されます（こうしんされます）
+        <br />
+        累積消費コイン（るいせきしょうひコイン）
+        <br />
+        未所属（みしょぞく）
+        <br />
+        <br />
+        崩壊3rd:...
+      </p>
+    </article>
+  </main>
+);
+
+export default Note;
