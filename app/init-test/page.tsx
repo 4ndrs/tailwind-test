41cabd9e858d54b1b83e2c9fe9f28b7afd9c6b28
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-03 08:02:37 +0200

    Add init-test

---
+++ app/init-test/page.tsx
@@ -0,0 +1,10 @@
+const InitTest = () => (
+  <>
+    <h1>Hello, World</h1>
+    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      Test
+    </button>
+  </>
+);
+
+export default InitTest;
