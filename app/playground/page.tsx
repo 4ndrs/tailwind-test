d7f6612bd7376e38664591f6fa23bcc7817d5775
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-04 06:38:31 +0200

    Add playground

---
+++ app/playground/page.tsx
@@ -0,0 +1,10 @@
+const Playground = () => (
+  <main className="flex w-screen flex-col items-center gap-3">
+    <h1 className="mt-3 text-3xl font-semibold text-slate-500">Playground</h1>
+    <button className="rounded-full bg-violet-500 px-5 py-2 font-semibold text-white hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700">
+      Save changes
+    </button>
+  </main>
+);
+
+export default Playground;
