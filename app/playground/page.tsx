4be7254639c84a2fdf896f95835436072076eac2
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-05 10:20:34 +0200

    Add playground list2

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -14,6 +14,7 @@ const Playground = () => (
 
     <List />
     <Table />
+    <List2 />
   </main>
 );
 
@@ -89,6 +90,46 @@ const Table = () => (
       ))}
     </tbody>
   </table>
+);
+
+const List2 = () => (
+  <ul>
+    {people.map((person) => (
+      <li
+        key={person.name}
+        className="group/item flex items-center justify-between rounded-xl p-4 hover:bg-slate-100"
+      >
+        <div className="flex gap-4">
+          <Image src={person.img} alt="" className="h-12 w-12 rounded-full" />
+          <div className="w-full">
+            <a className="font-semibold text-slate-900" href="#">
+              {person.name}
+            </a>
+            <div className="text-slate-500">{person.title}</div>
+          </div>
+        </div>
+        <a
+          href="#"
+          className="group/edit invisible flex items-start justify-between whitespace-nowrap rounded-full py-1 pl-4 pr-3 text-sm text-slate-500 transition hover:bg-slate-200 group-hover/item:visible"
+        >
+          <span className="font-semibold transition group-hover/edit:text-gray-700">
+            Call
+          </span>
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            className="h-5 w-5 text-slate-400 transition group-hover/edit:translate-x-0.5 group-hover/edit:text-slate-500"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </li>
+    ))}
+  </ul>
 );
 
 export default Playground;
